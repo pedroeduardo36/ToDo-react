5a8f8f366a52cfb8dae77cf3c9e93d1b9ba2637a
Pedro <pedrovilarinho.gpd@gmail.com>
2023-07-21 16:34:50 -0300

    Feat materialUI & Icons checkbox

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,19 +1,40 @@
 import styles from "./TaskCard.module.css";
 import trashLogo from "../assets/trash.svg";
+import { Checkbox } from "@mui/material";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+function handleCommentCheck(){
+  ''
+  // TEXT DECORATION -> THROUGH TEXT | COUNT +1 TASK DONE
+}
 
 export function TaskCard() {
   return (
     <div className={styles.cardWrapper}>
+      {/* <button className={styles.button}></button> */}
+      <Checkbox
+        size="medium"
+        icon={<RadioButtonUncheckedIcon />}
+        checkedIcon={<CheckCircleIcon />}
+        onChange={handleCommentCheck}
 
-        <button className={styles.button}></button>
+        sx={{
+          color: "#4ea8de",
+          "&.Mui-checked": {
+            color: "#8284fa",
+          },
+        }}
+      />
 
       <div className={styles.textWrapper}>
         <span>
-          Oi meu amor! Passando por meio dessa imagem pra dizer que te amo MUITO, e que estou morrendo de 
-          saudade de você. Estou doido pra te ver, cafungar e fazer carinho no xubinho. Te amo
+          Oi meu amor! Passando por meio dessa imagem pra dizer que te amo
+          MUITO, e que estou morrendo de saudade de você. Estou doido pra te
+          ver, cafungar e fazer carinho no xubinho. Te amo
         </span>
       </div>
-      
+
       <button>
         <img src={trashLogo} alt="trash logo" />
       </button>
